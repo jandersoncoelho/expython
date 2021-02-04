43eba3ad628c8a3ab6c3632759cab1ad759b304e
jandersoncoelho <jandersoncoelho@gmail.com>
2021-02-04 20:54:18 -0300

    alterar o arquivo

--- ex045.py
+++ ex045.py
@@ -10,3 +10,49 @@ Tesoura ganha do papel (cortando-o).
 Papel ganha da pedra (embrulhando-a).
 
 """
+from random import choice
+from time import sleep
+lista = ["Pedra", "Papel", "Tesoura"]
+escolha_computador = choice(lista)
+escolha_jogador = int(input('''
+Faça sua escolha.
+[ 1 ] Pedra
+[ 2 ] Papel
+[ 3 ] Tesoura 
+Opção: '''))
+print('JO')
+sleep(1)
+print('KEM')
+sleep(1)
+print('PÔ')
+print('=-' * 40)
+print(f'Computador escolheu {escolha_computador}')
+if escolha_jogador == 1: #Pedra
+    print('Usário escolheu Pedra.')
+    print('=-' * 40)
+    if escolha_computador == 'Pedra':
+        print('Computador e usuário empataram')
+    elif escolha_computador == 'Papel':
+        print('Computador venceu.')
+    elif escolha_computador == 'Tesoura':
+        print('Usuário Venceu.')
+elif escolha_jogador == 2: #Papel
+    print('Usário escolheu Papel.')
+    print('=-' * 40)
+    if escolha_computador == 'Pedra':
+        print('Computador venceu')
+    elif escolha_computador == 'Papel':
+        print('Computador e usuário empataram.')
+    elif escolha_computador == 'Tesoura':
+        print('Computador Venceu.')
+elif escolha_jogador == 3: #Tesoura
+    print('Usário escolheu Tesoura.')
+    print('=-' * 40)
+    if escolha_computador == 'Pedra':
+        print('Computador venceu.')
+    elif escolha_computador == 'Papel':
+        print('Usuário venceu.')
+    elif escolha_computador == 'Tesoura':
+        print('Computador e usuário empataram.')
+else:
+    print('Opção inválida! Tente novamente.')
