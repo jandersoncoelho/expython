2d36289fdf6636f38b5d38044d21ded3a9d59435
jandersoncoelho <jandersoncoelho@gmail.com>
2021-04-24 19:30:12 -0300

    aulas 59 Atualizada

--- ex059.py
+++ ex059.py
@@ -14,7 +14,7 @@ Exercício Python 059: Crie um programa que leia dois valores e mostre um menu n
 Seu programa deverá realizar a operação solicitada em cada caso.
 
 """
-
+from time import sleep
 print('-=-' * 20)
 print('CALCULADORA COM 2 NÚMEROS INFORMADOS'.center(60, '='))
 print('-=-' * 20)
@@ -35,24 +35,25 @@ while opção != 5:
     '''.center(100, ' '))
     opção = int(input('Escolha uma das opções exibidas: ').strip())
     print('-=-' * 20)
-    if 1 <= opção <= 5:
-        if opção == 1:
-            print(f'A soma entre {n1} e {n2} é {n1 + n2}'.center(60, ' '))
-        elif opção == 2:
-            print(f'O produto entre {n1} e {n2} é {n1 * n2}'.center(60, ' '))
-        elif opção == 3:
-            if n1 > n2:
-                print(f'{n1} é maior.'.center(60, ' '))
-            elif n2 > n1:
-                print(f'{n2} é maior.'.center(60, ' '))
-            else:
-                print('Os números informados são iguais.')
-        elif opção == 4:
-            n1 = int(input('Digite o primeiro valor inteiro: '))
-            n2 = int(input('Digite o segundo valor inteiro: '))
-        elif opção == 5:
-            opção = 5
-        # print('-=-' * 20)
+    if opção == 1:
+        print(f'A soma entre {n1} e {n2} é {n1 + n2}'.center(60, ' '))
+    elif opção == 2:
+        print(f'O produto entre {n1} e {n2} é {n1 * n2}'.center(60, ' '))
+    elif opção == 3:
+        if n1 > n2:
+            print(f'{n1} é maior.'.center(60, ' '))
+        elif n2 > n1:
+            print(f'{n2} é maior.'.center(60, ' '))
+        else:
+            print('Os números informados são iguais.')
+    elif opção == 4:
+        n1 = int(input('Digite o primeiro valor inteiro: '))
+        n2 = int(input('Digite o segundo valor inteiro: '))
+    elif opção == 5:
+        opção = 5
+        print('FINALIZANDO...')
     else:
         print(' Ooops! opção inválida! '.center(60, '-'))
+    sleep(2)
+
 
