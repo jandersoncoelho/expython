aa92e749dfb310082718f231a41f029e06f83dc0
jandersoncoelho <jandersoncoelho@gmail.com>
2023-08-20 09:30:49 -0300

    Atualização da fomatação do código

--- ex093.py
+++ ex093.py
@@ -16,9 +16,10 @@ Exercício Python 093:
 
 jogador = dict()
 partidas = list()
-    
+
 nome_jogador = str(input('Informe o nome do jogador: '))
-num_partidas = int(input(f'Informe o número de partidas do jogador {nome_jogador}: '))
+num_partidas = int(
+    input(f'Informe o número de partidas do jogador {nome_jogador}: '))
 
 for partida in range(1, num_partidas + 1):
     partidas.append(int(input(f'    Quantos gols feitos na {partida}ª? ')))
@@ -28,5 +29,5 @@ jogador['nome_jogador'] = nome_jogador
 jogador['total_partidas'] = sum(partidas)
 jogador['partidas'] = partidas[:]
 
-print('*' * 60)   
+print('*' * 60)
 print(jogador)
